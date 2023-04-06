392aa4f8edb6b4b1ce7a5a397566d19de5a42d15
Arman <md.armantext@gmail.com>
2023-04-06 11:52:53 +0600

    create a my first vue.js class

---
+++ day02/src/App.vue
@@ -0,0 +1,48 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import goal from './components/goal.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <!-- <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div> -->
+    <goal></goal>
+  </header>
+
+</template>
+
+<style scoped>
+/* header {
+  line-height: 1.5;
+  width: 100%;
+  background: red;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+} */
+</style>
---
+++ day02/src/components/goal.vue
@@ -0,0 +1,73 @@
+
+
+
+
+
+
+<script>
+  import { ref } from 'vue'
+export default {
+
+    setup() {
+         const id = ref('elka')
+
+        return {
+            roll : 500,
+            name : 'arman',
+            greading : 'hello everyone',
+            line:'hi birds',
+           tagline : '<small>i will see you</small>',
+           hack : "<a href='www.google.com'>this is link</a>",
+           headingID : 'heading02',
+           disabled: true,
+
+           objectOfAttrs : {
+                id: 'container',
+                class: 'wrapper',
+            }
+        };
+
+       
+        
+    },
+}
+</script>
+
+<template>
+    <div class="fotball">
+        <p :id="id">Arsenal:1-2:BVB</p>
+        <p>{{ name }}</p>
+        <p> <span v-html="greading" style="color:aqua"></span> </p>
+        <p v-bind:id="roll"></p>
+        <p v-text="line"></p>
+        <p v-html="tagline"></p>
+        <p v-html="hack"></p>
+        <p v-bind:id="headingID">Heading</p>
+        <button :disabled="disabled">click here</button>
+        <p v-bind="objectOfAttrs">s</p>
+    </div>
+    <div>
+        <p v-html="tagline" style="color:red"></p>
+    </div>
+</template>
+
+<style scoped>
+template{
+    width: 100%;
+}
+.fotball{
+    background: rgb(6, 129, 6);
+    width: 100%;
+}
+p{
+    font-size: 20px;
+    font-weight: bold;
+    color: white;
+}
+.color-red{
+    color: red;
+}
+#one{
+    background: rebeccapurple;
+}
+</style>
