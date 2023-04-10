f6002a5ea855c924d490d726846162db1dcba8db
Arman <md.armantext@gmail.com>
2023-04-10 11:36:52 +0600

    create a binding-style

---
+++ binding-style/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import goal from './components/goal.vue'
+import bindingStyle from './components/binding-style.vue';
+</script>
+
+<template>
+  <header>
+  
+    <binding-style></binding-style>
+  </header>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ binding-style/src/components/binding-style.vue
@@ -0,0 +1,40 @@
+
+
+<script>
+export default {
+    setup() {
+        return {
+            info : '#14C1AF',
+            font20 : '25px',
+            golore : '#1475C1',
+            styleObject : {
+                color : '#C12C14',
+                background : '#C19214',
+                padding : '28px',
+                marginTop : '10px',
+            },
+            secondObject : {
+                borderRadius : '5px',
+                fontStyle : 'italic',
+                color : 'white',
+            }
+        }
+    },
+}
+</script>
+
+<template>
+    <div>
+        <h3 v-bind:style="{
+            fontSize : font20, 
+            background : golore, 
+            color : '#C11414',  // direct css appaly
+        }">using the inline css</h3>
+        <h3 v-bind:style="styleObject">using the object style</h3>
+        <h3 v-bind:style="[styleObject , secondObject]">using the multipul object style</h3>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
