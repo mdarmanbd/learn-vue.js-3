763d6e8eaf6c6e6778778275f28d1cab789b7784
Arman <md.armantext@gmail.com>
2023-04-10 12:19:23 +0600

    create a conditional-rendering

---
+++ Conditional-rendering/src/components/conditional-rendering.vue
@@ -0,0 +1,30 @@
+
+
+
+
+<template>
+    <h2 v-if="number === 0">number is zero</h2>
+    <h2 v-else-if="number > 0">the number is positive</h2>
+    <h2 v-else-if="number < 0">the number is negative</h2>
+    <h2 v-else>not a number</h2>
+    <div v-if="condition">
+        <p>i am a conditiona</p>
+        <p>this is my fourth class</p>
+    </div>
+</template>
+
+<script>
+export default {
+    setup() {
+        return {
+           number : 'ko',
+           condition : true,
+        }
+    },
+}
+</script>
+
+
+<style scoped>
+
+</style>
