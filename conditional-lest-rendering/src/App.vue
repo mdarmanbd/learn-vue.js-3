a6afdb6cb6b57f061e94ebe80ecdc5ae7df19757
Arman <md.armantext@gmail.com>
2023-04-27 15:30:54 +0600

    add the conditional-lest-redering

---
+++ conditional-lest-rendering/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import conditionalRendering from './components/conditional-rendering.vue';
+
+</script>
+
+<template>
+  <header>
+  
+   <conditional-rendering></conditional-rendering>
+  </header>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ conditional-lest-rendering/src/components/conditional-rendering.vue
@@ -0,0 +1,34 @@
+
+
+
+
+<template>
+    <h2 v-if="number === 0">number is zero</h2>
+    <h2 v-else-if="number > 0">the number is positive</h2>
+    <h2 v-else-if="number < 0">the number is negative</h2>
+    <h2 v-else>not a number</h2>
+    <div v-if="condition">
+        <p>i am a conditiona</p>
+        <p>this is my fourth class</p>
+    </div>
+
+    <h2 v-show="showCondition">when the condition is false is hidden</h2>
+    <h2 v-if="showCondition">besic defference the inspect elements.when the condition is false ,it's ignor the line.</h2>
+</template>
+
+<script>
+export default {
+    setup() {
+        return {
+           number : 'ko',
+           condition : true,
+           showCondition : true,
+        }
+    },
+}
+</script>
+
+
+<style scoped>
+
+</style>
