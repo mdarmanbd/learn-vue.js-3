19324196107321b2147da092a2544613f0edc9e1
Arman <md.armantext@gmail.com>
2023-04-29 22:13:39 +0600

    attached the part02

---
+++ event-handling-part02/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+import day02 from './components/day02.vue';
+
+</script>
+
+<template>
+  <header>
+
+   <day-02></day-02>
+
+  </header>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ event-handling-part02/src/components/day02.vue
@@ -0,0 +1,68 @@
+
+
+<script >
+export default {
+    name: 'App',
+    setup() {
+        return {
+          count : 0,
+          name : 'hello',
+          
+        }
+    },
+    methods:{
+       changeName() {
+        this.name = 'rahul roy'
+       },
+       gopro(event) {
+        this.cout = 2,
+        console.log('Event',event)
+       }
+    }
+}
+</script>
+
+<template>
+
+   <h2>{{ count }}</h2>
+   <div>
+        <button @click="count += 1">increment</button>
+        <button @click="count -= 1">dicrement</button>
+        <button @click="gopro">dicrement consol</button>
+   </div>
+
+   <h2>{{ name }}</h2>
+   <div>
+    <button @mousemove="changeName">change name</button>
+   </div>
+   <div>
+
+   </div>
+</template>
+
+<style scoped>
+#app{
+    text-align: center;
+    background: red;
+}
+.day02{
+    width: 100%;
+    text-align: center;
+    background: rgb(235, 232, 232);
+}
+.underline{
+    text-decoration: underline;
+}
+.text-24{
+    font-size: 24px;
+}
+.text-italic{
+    font-style: italic;
+}
+.text-green{
+    color: rgb(17, 142, 17);
+}
+.text-red{
+    color: rgb(222, 6, 6);
+}
+</style>
